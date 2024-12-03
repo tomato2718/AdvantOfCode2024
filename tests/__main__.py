1cea4de2d5b085788c9193452dd133214a342997
tomato2718 <qweasdzxc030@gmail.com>
2024-12-03 10:54:06 +0800

    Feat: Enhance unittest framework

--- tests/__main__.py
+++ tests/__main__.py
@@ -1,10 +1,11 @@
 from . import day1
+from .helper import Testable
 
 
 def main() -> None:
     for test_name in day1.__all__:
-        test_func = getattr(day1, test_name)
-        test_func()
+        test_class: type[Testable] = getattr(day1, test_name)
+        test_class().run_tests()
 
 
 if __name__ == "__main__":
